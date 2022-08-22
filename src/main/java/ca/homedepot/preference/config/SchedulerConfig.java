c4e9b167944ee4393703edf5f55df4ec0646ed3b
graciela_rodelo_cardenas@homedepot.com <GRACIELA_RODELO_CARDENAS@homedepot.com>
2022-08-22 08:36:02 -0600

    Uncommenting line codes on SchedulerConfig class

--- src/main/java/ca/homedepot/preference/config/SchedulerConfig.java
+++ src/main/java/ca/homedepot/preference/config/SchedulerConfig.java
@@ -3,7 +3,9 @@ package ca.homedepot.preference.config;
 
 import javax.sql.DataSource;
 
+import ca.homedepot.preference.constants.PreferenceBatchConstants;
 import ca.homedepot.preference.service.impl.PreferenceServiceImpl;
+import ca.homedepot.preference.util.FileUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -17,7 +19,10 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.file.FlatFileHeaderCallback;
 import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -30,7 +35,16 @@ import ca.homedepot.preference.model.Registration;
 import ca.homedepot.preference.processor.EmailAnalyticsItemProcessor;
 import ca.homedepot.preference.processor.RegistrationItemProcessor;
 import ca.homedepot.preference.tasklet.BatchTasklet;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 
 /**
@@ -43,208 +57,234 @@ import org.springframework.transaction.PlatformTransactionManager;
 @EnableBatchProcessing
 
 @RequiredArgsConstructor
-public class SchedulerConfig extends DefaultBatchConfigurer
-{
-	/**
-	 * The Job builder factory.
-	 */
-	private final JobBuilderFactory jobBuilderFactory;
+public class SchedulerConfig extends DefaultBatchConfigurer {
+    /**
+     * The Job builder factory.
+     */
+    private final JobBuilderFactory jobBuilderFactory;
 
-	/**
-	 * The Step builder factory.
-	 */
-	private final StepBuilderFactory stepBuilderFactory;
+    /**
+     * The Step builder factory.
+     */
+    private final StepBuilderFactory stepBuilderFactory;
 
-	//Autowired
-	private DataSource dataSource;
+    //Autowired
+    private DataSource dataSource;
 
-	@Value("${analytic.file.registration}")
-	String registrationAnalyticsFile;
+    @Value("${analytic.file.registration}")
+    String registrationAnalyticsFile;
 
-	@Value("${analytic.file.email}")
-	String emailAnalyticsFile;
+    @Value("${analytic.file.email}")
+    String emailAnalyticsFile;
 
-	@Value("${process.analytics.chunk}")
-	Integer chunkValue;
+    @Value("${process.analytics.chunk}")
+    Integer chunkValue;
 
-	@Value("${sub.activity.days}")
-	Integer subactivity;
+    @Value("${sub.activity.days}")
+    Integer subactivity;
 
-	@Bean
-	public JdbcCursorItemReader<Registration> reader()
-	{
-		JdbcCursorItemReader<Registration> cursorItemReader = new JdbcCursorItemReader<Registration>();
-		cursorItemReader.setDataSource(dataSource);
-		/*
-		 * cursorItemReader
-		 * .setSql("SELECT createdts,article_id,action_type,email_id FROM registration_analytics WHERE DATE(createdts) = '" +
-		 * getActualDate() + "'"); cursorItemReader.setRowMapper(new RegistrationrowMapper());
-		 */
-		return cursorItemReader;
-	}
+    @Bean
+    public JdbcCursorItemReader<Registration> reader() {
+        JdbcCursorItemReader<Registration> cursorItemReader = new JdbcCursorItemReader<Registration>();
+        cursorItemReader.setDataSource(dataSource);
 
-	@Bean
-	public JdbcCursorItemReader<EmailAnalytics> emailreader()
-	{
-		JdbcCursorItemReader<EmailAnalytics> cursorItemReader = new JdbcCursorItemReader<EmailAnalytics>();
-		cursorItemReader.setDataSource(dataSource);
-		/*
-		 * cursorItemReader
-		 * .setSql("SELECT createdts,article_id,inventory,email_type,email_id FROM email_analytics WHERE DATE(createdts) = '" +
-		 * getActualDate() + "'"); cursorItemReader.setRowMapper(new EmailAnalyticsrowMapper());
-		 */
-		return cursorItemReader;
-	}
+        cursorItemReader
+                .setSql("SELECT createdts,article_id,action_type,email_id FROM registration_analytics WHERE DATE(createdts) = '" +
+                        getActualDate() + "'");
+        cursorItemReader.setRowMapper(new RegistrationrowMapper());
 
-	@Bean
-	public RegistrationItemProcessor processor()
-	{
-		return new RegistrationItemProcessor();
-	}
+        return cursorItemReader;
+    }
 
-	@Bean
-	public EmailAnalyticsItemProcessor emailprocessor()
-	{
-		return new EmailAnalyticsItemProcessor();
-	}
+    @Bean
+    public JdbcCursorItemReader<EmailAnalytics> emailreader() {
+        JdbcCursorItemReader<EmailAnalytics> cursorItemReader = new JdbcCursorItemReader<EmailAnalytics>();
+        cursorItemReader.setDataSource(dataSource);
 
-	@Bean
-	public FlatFileItemWriter<Registration> writer()
-	{
-		FlatFileItemWriter<Registration> writer = new FlatFileItemWriter<Registration>();
-		/*
-		 * writer.setResource(new FileSystemResource(getFile(registrationAnalyticsFile))); writer.setHeaderCallback(new
-		 * FlatFileHeaderCallback() { public void writeHeader(Writer writer) throws IOException {
-		 * writer.write("DATE | ARTICLE | ACTION | KEY"); } }); DelimitedLineAggregator<Registration> lineAggregator = new
-		 * DelimitedLineAggregator<Registration>(); lineAggregator.setDelimiter("|");
-		 *
-		 * BeanWrapperFieldExtractor<Registration> fieldExtractor = new BeanWrapperFieldExtractor<Registration>();
-		 * fieldExtractor.setNames(new String[] { PreferenceBatchConstants.CREATEDTS, PreferenceBatchConstants.ARTICLE_ID,
-		 * PreferenceBatchConstants.ACTION_TYPE, PreferenceBatchConstants.EMAIL_ID });
-		 * lineAggregator.setFieldExtractor(fieldExtractor);
-		 *
-		 * writer.setLineAggregator(lineAggregator);
-		 */
-		return writer;
-	}
+        cursorItemReader
+                .setSql("SELECT createdts,article_id,inventory,email_type,email_id FROM email_analytics WHERE DATE(createdts) = '" +
+                        getActualDate() + "'");
+        cursorItemReader.setRowMapper(new EmailAnalyticsrowMapper());
 
-	@Bean
-	public FlatFileItemWriter<EmailAnalytics> emailwriter()
-	{
-		FlatFileItemWriter<EmailAnalytics> writer = new FlatFileItemWriter<EmailAnalytics>();
-		/*
-		 * writer.setResource(new FileSystemResource(getFile(emailAnalyticsFile))); writer.setHeaderCallback(new
-		 * FlatFileHeaderCallback() { public void writeHeader(Writer writer) throws IOException {
-		 * writer.write("DATE | ARTICLE | INVENTORY | EMAILTYPE | KEY"); } }); DelimitedLineAggregator<EmailAnalytics>
-		 * lineAggregator = new DelimitedLineAggregator<EmailAnalytics>(); lineAggregator.setDelimiter("|");
-		 *
-		 * BeanWrapperFieldExtractor<EmailAnalytics> fieldExtractor = new BeanWrapperFieldExtractor<EmailAnalytics>();
-		 * fieldExtractor.setNames(new String[] { PreferenceBatchConstants.CREATEDTS, PreferenceBatchConstants.ARTICLE_ID,
-		 * PreferenceBatchConstants.INVENTORY, PreferenceBatchConstants.EMAIL_TYPE, PreferenceBatchConstants.EMAIL_ID });
-		 * lineAggregator.setFieldExtractor(fieldExtractor);
-		 *
-		 * writer.setLineAggregator(lineAggregator);
-		 */
-		return writer;
-	}
+        return cursorItemReader;
+    }
 
-	/**
-	 * The Job listener.
-	 */
-	private JobListener jobListener;
+    @Bean
+    public RegistrationItemProcessor processor() {
+        return new RegistrationItemProcessor();
+    }
 
-	/**
-	 * The Batch tasklet.
-	 */
-	private BatchTasklet batchTasklet;
+    @Bean
+    public EmailAnalyticsItemProcessor emailprocessor() {
+        return new EmailAnalyticsItemProcessor();
+    }
 
+    @Bean
+    public FlatFileItemWriter<Registration> writer() {
+        FlatFileItemWriter<Registration> writer = new FlatFileItemWriter<Registration>();
 
-	/**
-	 * The Transaction manager./*
-	 */
-	@Qualifier("visitorTransactionManager")
-	private final PlatformTransactionManager transactionManager;
+        writer.setResource(new FileSystemResource(getFile(registrationAnalyticsFile)));
+        writer.setHeaderCallback(new
+                                         FlatFileHeaderCallback() {
+                                             public void writeHeader(Writer writer) throws IOException {
+                                                 writer.write("DATE | ARTICLE | ACTION | KEY");
+                                             }
+                                         });
+        DelimitedLineAggregator<Registration> lineAggregator = new
+                DelimitedLineAggregator<Registration>();
+        lineAggregator.setDelimiter("|");
 
+        BeanWrapperFieldExtractor<Registration> fieldExtractor = new BeanWrapperFieldExtractor<Registration>();
+        fieldExtractor.setNames(new String[]{PreferenceBatchConstants.CREATEDTS, PreferenceBatchConstants.ARTICLE_ID,
+                PreferenceBatchConstants.ACTION_TYPE, PreferenceBatchConstants.EMAIL_ID});
+        lineAggregator.setFieldExtractor(fieldExtractor);
 
-	/**
-	 * Process job.
-	 *
-	 * @return the job
-	 */
+        writer.setLineAggregator(lineAggregator);
+
+        return writer;
+    }
+
+    @Bean
+    public FlatFileItemWriter<EmailAnalytics> emailwriter() {
+        FlatFileItemWriter<EmailAnalytics> writer = new FlatFileItemWriter<EmailAnalytics>();
+
+        writer.setResource(new FileSystemResource(getFile(emailAnalyticsFile)));
+        writer.setHeaderCallback(new
+                                         FlatFileHeaderCallback() {
+                                             public void writeHeader(Writer writer) throws IOException {
+                                                 writer.write("DATE | ARTICLE | INVENTORY | EMAILTYPE | KEY");
+                                             }
+                                         });
+        DelimitedLineAggregator<EmailAnalytics>
+                lineAggregator = new DelimitedLineAggregator<EmailAnalytics>();
+        lineAggregator.setDelimiter("|");
+
+        BeanWrapperFieldExtractor<EmailAnalytics> fieldExtractor = new BeanWrapperFieldExtractor<EmailAnalytics>();
+        fieldExtractor.setNames(new String[]{PreferenceBatchConstants.CREATEDTS, PreferenceBatchConstants.ARTICLE_ID,
+                PreferenceBatchConstants.INVENTORY, PreferenceBatchConstants.EMAIL_TYPE, PreferenceBatchConstants.EMAIL_ID});
+        lineAggregator.setFieldExtractor(fieldExtractor);
+
+        writer.setLineAggregator(lineAggregator);
+
+        return writer;
+    }
+
+    /**
+     * The Job listener.
+     */
+    private JobListener jobListener;
+
+    /**
+     * The Batch tasklet.
+     */
+    private BatchTasklet batchTasklet;
 
 
-	@Bean
-	public Job processJob()
-	{
-
-		return jobBuilderFactory.get("processJob").incrementer(new RunIdIncrementer()).listener(jobListener).start(orderStep1())
-				.build();
-
-	}
-
-	/**
-	 * Order step 1 step.
-	 *
-	 * @return the step
-	 */
-
-	@Bean
-	public Step orderStep1()
-	{
-		return stepBuilderFactory.get("orderStep1").<Registration, Registration> chunk(chunkValue).reader(reader())
-				.processor(processor()).writer(writer()).build();
-	}
-
-	@Bean
-	public Step orderStep4()
-	{
-
-		return stepBuilderFactory.get("orderStep4").tasklet(batchTasklet).transactionManager(transactionManager).build();
-
-	}
+    /**
+     * The Transaction manager./*
+     */
+    @Qualifier("visitorTransactionManager")
+    private final PlatformTransactionManager transactionManager;
 
 
-	/*
-	 * }
-	 *
-	 * @Bean public Step orderStep2() { return stepBuilderFactory.get("orderStep2").<EmailAnalytics, EmailAnalytics>
-	 * chunk(chunkValue).reader(emailreader()) .processor(emailprocessor()).writer(emailwriter()).build(); }
-	 *
-	 * @Bean public Step orderStep3() { return
-	 * stepBuilderFactory.get("orderStep3").tasklet(uploadTasklet).transactionManager(transactionManager).build(); }
-	 *
-	 *
-	 *
-	 * 
-	 *//**
-		 * {@inheritDoc}
-		 *
-		 * override to do not set data a source. Spring initialize will use a Map based JobRepository instead of database. This
-		 * is to avoid spring batch from creating batch related metadata tables in the scheduling schemas.
-		 */
-	/*
-	 * @Override public void setDataSource(DataSource dataSource) { }
-	 *
-	 *//**
-		 * Getting actual date for processing.
-		 */
-	/*
-	 * private String getActualDate() { SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd"); Instant now =
-	 * Instant.now(); Instant yesterday = now.minus(subactivity, ChronoUnit.DAYS); return sdf.format(Date.from(yesterday));
-	 * }
-	 *
-	 *//**
-		 * Gets file name.
-		 *
-		 * @param baseName
-		 *           the base name
-		 * @return the file name
-		 *//*
-			 * private String getFile(String baseName) { Date currentDate = new Date(); SimpleDateFormat sdf = new
-			 * SimpleDateFormat("ddMMyyyyhhmmss"); String fileName = baseName + "_" + sdf.format(currentDate) + ".csv"; if
-			 * (registrationAnalyticsFile.equals(baseName)) { FileUtil.setRegistrationFile(fileName); } else {
-			 * FileUtil.setEmailanalyticsFile(fileName); } File file = new File(fileName); try { file.createNewFile(); } catch
-			 * (IOException e) { log.error("Error while creating file " + fileName); } return file.getName(); }
-			 */
+    /**
+     * Process job.
+     *
+     * @return the job
+     */
+
+
+    @Bean
+    public Job processJob() {
+
+        return jobBuilderFactory.get("processJob").incrementer(new RunIdIncrementer()).listener(jobListener).start(orderStep1())
+                .build();
+
+    }
+
+    /**
+     * Order step 1 step.
+     *
+     * @return the step
+     */
+
+    @Bean
+    public Step orderStep1() {
+        return stepBuilderFactory.get("orderStep1").<Registration, Registration>chunk(chunkValue).reader(reader())
+                .processor(processor()).writer(writer()).build();
+    }
+
+    @Bean
+    public Step orderStep4() {
+
+        return stepBuilderFactory.get("orderStep4").tasklet(batchTasklet).transactionManager(transactionManager).build();
+
+    }
+
+
+    @Bean
+    public Step orderStep2() {
+        return stepBuilderFactory.get("orderStep2").<EmailAnalytics, EmailAnalytics>
+                chunk(chunkValue).reader(emailreader()).processor(emailprocessor()).writer(emailwriter()).build();
+    }
+    /*
+     * @Bean public Step orderStep3() { return
+     * stepBuilderFactory.get("orderStep3").tasklet(uploadTasklet).transactionManager(transactionManager).build(); }
+     *
+     *
+     *
+     *
+     */
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * override to do not set data a source. Spring initialize will use a Map based JobRepository instead of database. This
+     * is to avoid spring batch from creating batch related metadata tables in the scheduling schemas.
+     */
+
+    @Override
+    public void setDataSource(DataSource dataSource) {
+    }
+
+    /**
+     * Getting actual date for processing.
+     */
+
+    private String getActualDate() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Instant now =
+                Instant.now();
+        Instant yesterday = now.minus(subactivity, ChronoUnit.DAYS);
+        return sdf.format(Date.from(yesterday));
+    }
+
+    /**
+     * Gets file name.
+     *
+     * @param baseName the base name
+     * @return the file name
+     */
+    private String getFile(String baseName) {
+        Date currentDate = new Date();
+        SimpleDateFormat sdf = new
+                SimpleDateFormat("ddMMyyyyhhmmss");
+        String fileName = baseName + "_" + sdf.format(currentDate) + ".csv";
+        if
+        (registrationAnalyticsFile.equals(baseName)) {
+            FileUtil.setRegistrationFile(fileName);
+        } else {
+            FileUtil.setEmailanalyticsFile(fileName);
+        }
+        File file = new File(fileName);
+        try {
+            file.createNewFile();
+        } catch
+        (IOException e) {
+            log.error("Error while creating file " + fileName);
+        }
+        return file.getName();
+    }
+
 
 }
