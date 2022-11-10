7375b04887ef8ee0165d652fcad0b50343f089b8
Juan Lara <juan.lara@neoris.com>
2022-11-10 13:43:09 -0600

    Fix Test Case

--- src/test/java/ca/homedepot/preference/processor/InternalOutboundProcessorTest.java
+++ src/test/java/ca/homedepot/preference/processor/InternalOutboundProcessorTest.java
@@ -8,6 +8,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import java.math.BigDecimal;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InternalOutboundProcessorTest
@@ -22,12 +27,20 @@ class InternalOutboundProcessorTest
 	@Test
 	void process() throws Exception
 	{
+		Format formatter = new SimpleDateFormat("yyyy-MM-dd HH:MM:SS");
 		String split = SourceDelimitersConstants.DELIMITER_COMA;
+		Date date = new Date();
 
 		internalOutboundDto = Mockito.mock(InternalOutboundDto.class);
 		Mockito.when(internalOutboundDto.getEmailAddr()).thenReturn("test@test.com");
+		Mockito.when(internalOutboundDto.getCanPtcEffectiveDate()).thenReturn(date);
+		Mockito.when(internalOutboundDto.getEarlyOptInIDate()).thenReturn(date);
+		Mockito.when(internalOutboundDto.getEmailStatus()).thenReturn(BigDecimal.ZERO);
 		InternalOutboundProcessor processor = new InternalOutboundProcessor();
 		InternalOutboundProcessorDto test = processor.process(internalOutboundDto);
 		assertEquals(test.getEmailAddr(), ("test@test.com" + split));
+		assertEquals(test.getCanPtcEffectiveDate(), formatter.format(date) + split);
+		assertEquals(test.getEarlyOptInDate(), formatter.format(date) + split);
+		assertEquals(test.getEmailStatus(), ("00" + split));
 	}
 }
