63274f4ec22971579e170a062a61348df29acfff
Juan Lara <juan.lara@neoris.com>
2022-12-20 19:20:14 -0600

    Solving issue with error counter

--- src/main/java/ca/homedepot/preference/processor/ExactTargetEmailProcessor.java
+++ src/main/java/ca/homedepot/preference/processor/ExactTargetEmailProcessor.java
@@ -22,6 +22,10 @@ public class ExactTargetEmailProcessor implements ItemProcessor<EmailOptOuts, Fi
 
 	private int count = 0;
 
+	public void setCount(int count) {
+		this.count = count;
+	}
+
 	/**
 	 * Process the item from LayoutB (SFMC)
 	 * 
--- src/main/java/ca/homedepot/preference/processor/RegistrationItemProcessor.java
+++ src/main/java/ca/homedepot/preference/processor/RegistrationItemProcessor.java
@@ -23,6 +23,10 @@ public class RegistrationItemProcessor implements ItemProcessor<InboundRegistrat
 
 	private int count = 0;
 
+	public void setCount(int count) {
+		this.count = count;
+	}
+
 	/**
 	 * Constructor with resource
 	 *
