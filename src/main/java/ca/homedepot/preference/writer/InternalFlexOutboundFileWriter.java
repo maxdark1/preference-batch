98c7669c797aff1eca339da3d1ae19e8a4776b36
GXR8I3O <graciela_rodelo_cardenas@homedepot.com>
2022-12-22 09:23:07 -0700

    PCAM-382: solving PMD issue

--- src/main/java/ca/homedepot/preference/writer/InternalFlexOutboundFileWriter.java
+++ src/main/java/ca/homedepot/preference/writer/InternalFlexOutboundFileWriter.java
@@ -19,7 +19,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.math.BigDecimal;
 import java.text.Format;
 import java.text.SimpleDateFormat;
