7590c9a185c953c5f8eb40f1c1d4428b13c053c4
Juan Lara <juan.lara@neoris.com>
2023-02-09 12:15:13 -0600

    Fixing coverage #3

--- src/main/java/ca/homedepot/preference/PreferenceBatchApplication.java
+++ src/main/java/ca/homedepot/preference/PreferenceBatchApplication.java
@@ -1,11 +1,13 @@
 package ca.homedepot.preference;
 
 
+import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@Generated
 public class PreferenceBatchApplication
 {
 	public static void main(String[] args)
