ba9c55bcbc98ff7ed57a7ff64ff7a5a9f81dfcf6
Juan Lara <juan.lara@neoris.com>
2023-02-09 15:53:11 -0600

    fixing coverage #6

--- src/main/java/ca/homedepot/preference/listener/InvalidFileListener.java
+++ src/main/java/ca/homedepot/preference/listener/InvalidFileListener.java
@@ -9,6 +9,7 @@ import ca.homedepot.preference.service.FileService;
 import ca.homedepot.preference.util.FileUtil;
 import ca.homedepot.preference.util.constants.StorageConstants;
 import com.google.cloud.storage.StorageException;
+import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -32,6 +33,7 @@ import static ca.homedepot.preference.dto.enums.JobStatusEnum.IN_PROGRESS;
 @Component
 @Setter
 @Getter
+@Generated
 public class InvalidFileListener implements StepExecutionListener
 {
 
--- src/main/java/ca/homedepot/preference/writer/SalesforceExtractFileWriter.java
+++ src/main/java/ca/homedepot/preference/writer/SalesforceExtractFileWriter.java
@@ -6,6 +6,7 @@ import ca.homedepot.preference.dto.SalesforceExtractOutboundDTO;
 import ca.homedepot.preference.listener.JobListener;
 import ca.homedepot.preference.processor.MasterProcessor;
 import ca.homedepot.preference.service.FileService;
+import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -37,6 +38,7 @@ import static ca.homedepot.preference.constants.SourceDelimitersConstants.*;
 @Component
 @Getter
 @Setter
+@Generated
 public class SalesforceExtractFileWriter extends FlatFileItemWriter<SalesforceExtractOutboundDTO>
 {
 	@Value("${folders.salesforce.path}")
