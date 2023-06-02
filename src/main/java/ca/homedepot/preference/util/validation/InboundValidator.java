f415d4b1efb6dc8af8fddd3012467ad27a61ec79
GXR8I3O <graciela_rodelo_cardenas@homedepot.com>
2023-06-02 10:48:06 -0700

    PCAM-460: Editing the Validation day method

--- src/main/java/ca/homedepot/preference/util/validation/InboundValidator.java
+++ src/main/java/ca/homedepot/preference/util/validation/InboundValidator.java
@@ -199,7 +199,7 @@ public class InboundValidator
 	public static void validateMonth(int month, StringBuilder error)
 	{
 		if (!(month >= 1 && month <= 12))
-			error.append(" Invalid Month: ").append(month).append(" ,");
+			error.append("  Validation date error - invalid Month: ").append(month).append(" ,");
 	}
 
 	/**
@@ -214,14 +214,14 @@ public class InboundValidator
 	{
 		GregorianCalendar calendar = new GregorianCalendar();
 		int maxDays = 31;
-		if (month == 4 || month == 5 || month == 9 || month == 11)
+		if (month == 4 || month == 6 || month == 9 || month == 11)
 			maxDays = 30;
 		if (month == 2)
 		{
 			maxDays = calendar.isLeapYear(year) ? 29 : 28;
 		}
 		if (day < 1 || day > maxDays)
-			error.append(" Invalid day: ").append(day).append(",");
+			error.append(" Validation date error - invalid day: ").append(day).append(",");
 	}
 
 	/**
